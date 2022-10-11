da397b75aaf7657e91f11a3f59c0224bccbaddce
Vishal <vishal@openspaceservices.com>
2022-10-11 18:13:36 +0530

    using model_to_deict method and jsonResponse

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
-from django.http import HttpResponse
+from django.http import JsonResponse
+from django.forms.models import model_to_dict
 from django.shortcuts import render
 
 from blog.models import Post,Category
@@ -8,9 +9,11 @@ from blog.models import Post,Category
 def get_all_blogs(request):
     if request.method == "GET":
         posts = Post.objects.all()
-        return HttpResponse(posts[0].title)
+        return JsonResponse(posts[0].title)
 
 def get_blog_with_id(request,id):
     if request.method == "GET":
-        posts = Post.objects.get(pk=id)
-        return HttpResponse(posts.body)
+        post = Post.objects.get(pk=id)
+        data = {}
+        data = model_to_dict(post)
+        return JsonResponse(data)
