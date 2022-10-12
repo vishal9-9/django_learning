3f4bf6dc1aee4829ad8d059cb7f27c453659daae
Vishal <vishal@openspaceservices.com>
2022-10-12 11:27:50 +0530

    using api_view decorator and rest_framework response instead of JsonResponse

--- blog/views.py
+++ blog/views.py
@@ -1,19 +1,29 @@
 from django.http import JsonResponse
 from django.forms.models import model_to_dict
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 from blog.models import Post,Category
 
 # Create your views here.
 
+@api_view(['GET'])
 def get_all_blogs(request):
     if request.method == "GET":
         posts = Post.objects.all()
-        return JsonResponse(posts[0].title)
+        data = []
+        if posts:
+            for i in posts:
+                data.append(model_to_dict(i,fields=['id','title','body','created_on']))
+        return Response(data=data)
+        # return JsonResponse({"message":data},safe=False)
 
+@api_view(['GET'])
 def get_blog_with_id(request,id):
     if request.method == "GET":
         post = Post.objects.get(pk=id)
         data = {}
-        data = model_to_dict(post)
-        return JsonResponse(data)
+        data = model_to_dict(post,fields=['id','title','body'])
+        return Response(data=data)
+        # return JsonResponse({"message":data},safe=False)
